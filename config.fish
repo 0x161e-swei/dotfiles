20dd40843cc38153906860dc31e9ba9e0015bb96
Shijia Wei <swei.utexas@gmail.com>
2020-05-18 16:38:24 -0500

    add simple bobthefish config

---
+++ config.fish
@@ -0,0 +1,10 @@
+set -g theme_nerd_fonts yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_date_timezone America/Chicago
+set -g theme_newline_cursor yes
+set -g theme_color_scheme dracula
+set -g theme_show_exit_status yes
+
+alias l 'ls'
